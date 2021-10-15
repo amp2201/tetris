26ce650d2a1585bf292ef234d6225b760d1b7dad
Drew Pavlik <63932180+amp2201@users.noreply.github.com>
2021-10-14 23:47:04 -0400

    Update Board.java

--- src/main/java/cs1302/tetris/Board.java
+++ src/main/java/cs1302/tetris/Board.java
@@ -14,16 +14,16 @@ import javafx.scene.image.Image;
 
 public class Board extends TilePane {
 
-    private Rectangle[][] blanks;
-    private Boolean[][] state;
-    private Rectangle[][] content;
-    private Group[][] holders;
+    private Rectangle[][] blanks;       // placeholders for empty spaces of the board
+    private Boolean[][] state;          // keeps track of which spaces of the board are filled
+    private Rectangle[][] content;      // the actual content shown to the user
+    private Group[][] holders;          // Group nodes that will each render an element of content[][]
 
     private ImageView nextImage;
     private ImageView heldImage;
     private Tetramino active;
     private Rectangle[] activeBlocks;
-    private int[][] aL;
+    private int[][] aL;                 // aL stands for active locations; it keeps track of the row and column of the 4 blocks of the Tetramino that is currently in play
     private Boolean canHold;
 
     private Timeline fallLine;
@@ -54,9 +54,9 @@ public class Board extends TilePane {
         activeBlocks = new Rectangle[4];
         orientation = "";
         for (int i = 0; i < 20; i++) {
-
             for (int j = 0; j < 10; j++) {
 
+                // initialize array that holds the content of the board's blank spaces
                 blanks[i][j] = new Rectangle();
                 blanks[i][j].setFill(Color.WHITE);
                 blanks[i][j].setStroke(Color.SLATEGRAY);
@@ -64,38 +64,35 @@ public class Board extends TilePane {
                 blanks[i][j].setWidth(25);
             }
         }
-        /*
-        blanks[0][3].setFill(Color.LIGHTCORAL);
-        blanks[0][4].setFill(Color.LIGHTCORAL);
-        blanks[0][5].setFill(Color.LIGHTCORAL);
-        blanks[0][6].setFill(Color.LIGHTCORAL);
-        blanks[1][3].setFill(Color.LIGHTCORAL);
-        blanks[1][4].setFill(Color.LIGHTCORAL);
-        blanks[1][5].setFill(Color.LIGHTCORAL);
-        blanks[1][6].setFill(Color.LIGHTCORAL);
-        */
+        
         state = new Boolean[20][10];
         content = new Rectangle[20][10];
-        holders = new Group[20][10];
+        holders = new Group[20][10];    
 
         for (int i = 0; i < 20; i++) {
-
             for (int j = 0; j < 10; j++) {
 
-                state[i][j] = false;
+                // initialize the board to be empty
+                state[i][j] = false; 
                 holders[i][j] = new Group();
                 content[i][j] = blanks[i][j];
                 holders[i][j].getChildren().add(content[i][j]);
                 this.getChildren().add(holders[i][j]);
             }
         }
-    } // constructor
+    }
 
+    /**
+     * Loads in a new Tetramino at the top of the board. If the top of the board is already full when the Tetramino is loaded,
+     * it is game over.
+     */
     public void loadIn(int next) {
 
         active = new Tetramino(next);
-
+        
         nextInt = active.getNextShape();
+        
+        // swap out the image for the next Tetramino
         if (nextInt == 0) {
             nextImage.setImage(new Image("file:resources/yellow.png"));
         } else if (nextInt == 1) {
@@ -111,18 +108,22 @@ public class Board extends TilePane {
         } else if (nextInt == 6) {
             nextImage.setImage(new Image("file:resources/red.png"));
         }
+        
         aL = active.getInitialLocation();
         activeBlocks = active.getBlocks();
         orientation = "up";
-        // checks for gameover
+        
+        // check for game over when the initial locations of the new Tetramino are already filled by another Tetramino's blocks
         if (state[aL[0][0]][aL[0][1]] == true ||
-        state[aL[1][0]][aL[1][1]] == true ||
-        state[aL[2][0]][aL[2][1]] == true ||
-        state[aL[3][0]][aL[3][1]]) {
+          state[aL[1][0]][aL[1][1]] == true ||
+          state[aL[2][0]][aL[2][1]] == true ||
+          state[aL[3][0]][aL[3][1]]) {
             System.out.println("Game over");
             fallLine.stop();
             return;
         }
+        
+        // swap out empty squares on the board with the blocks of the new Tetramino
         content[aL[0][0]][aL[0][1]] = activeBlocks[0];
         content[aL[1][0]][aL[1][1]] = activeBlocks[1];
         content[aL[2][0]][aL[2][1]] = activeBlocks[2];
@@ -130,8 +131,12 @@ public class Board extends TilePane {
         updateBoard();
     }
 
+    /*
+     * Update the visual state of the board by rendering its new content.
+     */
     public void updateBoard() {
 
+        // replace the children of all the Group nodes with the newly updated elements from content[][]
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 10; j++) {
 
@@ -142,30 +147,39 @@ public class Board extends TilePane {
     }
 
 
+    /**
+     * Moves all blocks of a Tetramino down by 1 row.
+     */
     public void fall() {
 
+        // if the bottom of the board or a block is directly below the falling Tetramino, the falling should stop
         if (aL[0][0] == 19 || aL[1][0] == 19 || aL[2][0] == 19 || aL[3][0] == 19 ||
-        state[aL[0][0] + 1][aL[0][1]] == true || // if there is a piece below a block, don't fa
-        state[aL[1][0] + 1][aL[1][1]] == true ||
-        state[aL[2][0] + 1][aL[2][1]] == true ||
-        state[aL[3][0] + 1][aL[3][1]] == true) {
-            //fallLine.stop()
-//            moveLeft();
+          state[aL[0][0] + 1][aL[0][1]] == true || 
+          state[aL[1][0] + 1][aL[1][1]] == true ||
+          state[aL[2][0] + 1][aL[2][1]] == true ||
+          state[aL[3][0] + 1][aL[3][1]] == true) {
+            
+            // mark that the squares that the Tetramino landed in are now occupied
             state[aL[0][0]][aL[0][1]] = true;
             state[aL[1][0]][aL[1][1]] = true;
             state[aL[2][0]][aL[2][1]] = true;
             state[aL[3][0]][aL[3][1]] = true;
+            
             clearLines();
             canHold = true;
-            loadIn(active.getNextShape());
+            loadIn(active.getNextShape());  // this Tetramino is now in place, so load the next Tetramino to fall
 
             //fallLine.play();
             return;
         }
+        
+        // replace the squares that the Tetramino will fall from with blanks
         content[aL[0][0]][aL[0][1]] = blanks[aL[0][0]][aL[0][1]];
         content[aL[1][0]][aL[1][1]] = blanks[aL[1][0]][aL[1][1]];
         content[aL[2][0]][aL[2][1]] = blanks[aL[2][0]][aL[2][1]];
         content[aL[3][0]][aL[3][1]] = blanks[aL[3][0]][aL[3][1]];
+        
+        // advance all four blocks of the active Tetramino to the next row
         aL[0][0]++;
         aL[1][0]++;
         aL[2][0]++;
@@ -177,11 +191,13 @@ public class Board extends TilePane {
         updateBoard();
     }
 
+    /**
+     * Set up an event handler to call the fall function every second on a separate thread.
+     */
     public void play() {
 
         loadIn(-1);
         //moveLeft();
-
 
         fallFrame = new KeyFrame(Duration.seconds(1), faller);
         fallLine = new Timeline();
@@ -191,21 +207,28 @@ public class Board extends TilePane {
         //fallLine.play();
     }
 
+    /**
+     * Shift all blocks of a Tetramino to the left by one column, provided that the border or other blocks are not in the way.
+     */
     public void moveLeft() {
 
+        // if a block is already in the leftmost column or another block is to the left of this Tetramino, this Tetramino can't move left
         if (aL[0][1] == 0 || aL[1][1] == 0 || aL[2][1] == 0 || aL[3][1] == 0 ||
-        state[aL[0][0]][aL[0][1] - 1] == true ||
-        state[aL[1][0]][aL[1][1] - 1] == true ||
-        state[aL[2][0]][aL[2][1] - 1] == true ||
-        state[aL[3][0]][aL[3][1] - 1] == true) {
+          state[aL[0][0]][aL[0][1] - 1] == true ||
+          state[aL[1][0]][aL[1][1] - 1] == true ||
+          state[aL[2][0]][aL[2][1] - 1] == true ||
+          state[aL[3][0]][aL[3][1] - 1] == true) {
 
             return;
         }
 
+        // replace the spaces that the Tetramino will move left from with blank spaces
         content[aL[0][0]][aL[0][1]] = blanks[aL[0][0]][aL[0][1]];
         content[aL[1][0]][aL[1][1]] = blanks[aL[1][0]][aL[1][1]];
         content[aL[2][0]][aL[2][1]] = blanks[aL[2][0]][aL[2][1]];
         content[aL[3][0]][aL[3][1]] = blanks[aL[3][0]][aL[3][1]];
+        
+        // move all blocks of the Tetramino to the left
         aL[0][1]--;
         aL[1][1]--;
         aL[2][1]--;
@@ -217,21 +240,29 @@ public class Board extends TilePane {
         updateBoard();
     }
 
+    /**
+     * Shift all blocks of a Tetramino to the right, provided that the right border or other blocks are not in the way.
+     */ 
     public void moveRight() {
 
+        // if a block is already in the rightmost column or another block is to the right of this Tetramino,
+        // this Tetramino cannot move right
         if (aL[0][1] == 9 || aL[1][1] == 9 || aL[2][1] == 9 || aL[3][1] == 9 ||
-        state[aL[0][0]][aL[0][1] + 1] == true ||
-        state[aL[1][0]][aL[1][1] + 1] == true ||
-        state[aL[2][0]][aL[2][1] + 1] == true ||
-        state[aL[3][0]][aL[3][1] + 1] == true) {
+          state[aL[0][0]][aL[0][1] + 1] == true ||
+          state[aL[1][0]][aL[1][1] + 1] == true ||
+          state[aL[2][0]][aL[2][1] + 1] == true ||
+          state[aL[3][0]][aL[3][1] + 1] == true) {
 
             return;
         }
 
+        // replace the spaces that the Tetramino will move right from with blank spaces
         content[aL[0][0]][aL[0][1]] = blanks[aL[0][0]][aL[0][1]];
         content[aL[1][0]][aL[1][1]] = blanks[aL[1][0]][aL[1][1]];
         content[aL[2][0]][aL[2][1]] = blanks[aL[2][0]][aL[2][1]];
         content[aL[3][0]][aL[3][1]] = blanks[aL[3][0]][aL[3][1]];
+        
+        // move all 4 blocks of the Tetramino to the right
         aL[0][1]++;
         aL[1][1]++;
         aL[2][1]++;
@@ -243,6 +274,7 @@ public class Board extends TilePane {
         updateBoard();
     }
 
+    
     public static void runNow(Runnable target) {
 
         Thread t = new Thread(target);
@@ -250,10 +282,14 @@ public class Board extends TilePane {
         t.start();
     }
 
+    /**
+     * Rotate the purple Tetramino's blocks the left, provided that it has the available space.
+     */
     public void rotateLeftPurple() {
 
         int rowCenter = aL[1][0];
         int colCenter = aL[1][1];
+        
         if (orientation.equals("up")) {
 
             if (aL[1][0] == 19 || state[rowCenter + 1][colCenter] == true) {
@@ -897,6 +933,7 @@ public class Board extends TilePane {
             clear = true;
             for (int j = 0; j < 10; j++) {
 
+                // if there is a space in the row that is not filled by a Tetramino, do not clear the line
                 if (state[i][j] == false) {
                     clear = false;
                     break;
@@ -922,11 +959,14 @@ public class Board extends TilePane {
                     state[0][h] = false;
                 }
                 updateBoard();
+                
                 /*try {
                     Thread.sleep(2000);
                 } catch (Exception e) {
                     System.out.print("");
                     }*/
+                
+                // increase the speed of the game as the user clears more lines
                 if (speedUp) {
                     if (tempClearedLines < 10 && clearedLines >= 10) {
                         fallFrame = new KeyFrame(Duration.seconds(.9), faller);
@@ -952,19 +992,29 @@ public class Board extends TilePane {
         return fallLine;
     }
 
+    /**
+     *  Lets the user save the active Tetramino to be used later. If a Tetramino is already held, the held Tetramino will
+     *  become the active Tetramino.
+     */
     public void hold() {
 
         if (canHold) {
+            
+            // replace the spaces occupied by the active Tetramino with blanks
             content[aL[0][0]][aL[0][1]] = blanks[aL[0][0]][aL[0][1]];
             content[aL[1][0]][aL[1][1]] = blanks[aL[1][0]][aL[1][1]];
             content[aL[2][0]][aL[2][1]] = blanks[aL[2][0]][aL[2][1]];
             content[aL[3][0]][aL[3][1]] = blanks[aL[3][0]][aL[3][1]];
+            
+            // if a piece is not already being held
             if (held == -1) {
+                
                 held = active.getIntShape();
                 int tempNext = active.getNextShape();
-                active = new Tetramino(tempNext);
-
-                nextInt = active.getNextShape();
+                active = new Tetramino(tempNext);   // create a new Tetramino
+                
+                // swap out the image for the next Tetramino
+                nextInt = active.getNextShape();    
                 if (nextInt == 0) {
                     nextImage.setImage(new Image("file:resources/yellow.png"));
                 } else if (nextInt == 1) {
@@ -981,18 +1031,21 @@ public class Board extends TilePane {
                     nextImage.setImage(new Image("file:resources/red.png"));
                 }
 
-
                 aL = active.getInitialLocation();
                 activeBlocks = active.getBlocks();
                 orientation = "up";
+                
+                // show the new Tetramino at the top of the board
                 content[aL[0][0]][aL[0][1]] = activeBlocks[0];
                 content[aL[1][0]][aL[1][1]] = activeBlocks[1];
                 content[aL[2][0]][aL[2][1]] = activeBlocks[2];
                 content[aL[3][0]][aL[3][1]] = activeBlocks[3];
                 updateBoard();
-            } else {
+                
+            } else { // there is already a Tetramino being held
+                
                 int tempHeld = active.getIntShape();
-                active = new Tetramino(held, nextInt);
+                active = new Tetramino(held, nextInt);  // the held Tetramino becomes the active Tetramino
                 held = tempHeld;
                 aL = active.getInitialLocation();
                 activeBlocks = active.getBlocks();
@@ -1004,6 +1057,8 @@ public class Board extends TilePane {
                 updateBoard();
             }
             canHold = false;
+            
+            // swap out the image for the held Tetramino
             if (held == 0) {
                 heldImage.setImage(new Image("file:resources/yellow.png"));
             } else if (held == 1) {
